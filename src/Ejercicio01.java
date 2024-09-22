be1e05dd5b4db7e87b9220d6d2b27b9a525b19e3
jgomezz <jgomezz@gmail.com>
2024-09-22 10:58:17 -0500

    first version

---
+++ src/Ejercicio01.java
@@ -0,0 +1,15 @@
+import java.security.DomainCombiner;
+
+public class Ejercicio01 {
+	
+	public static void main(String[] args) {
+		
+		 String nombre = "Pedro";
+		 int numeroFavorito = 7;
+		 boolean esDomingo = true;
+		 int anhoActual = 2024;
+		 
+		 System.out.println("Mi nombre es " + nombre);
+	}
+
+}

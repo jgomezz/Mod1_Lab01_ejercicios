d66d67771336da70d386d244ae37affe6b34ff54
jgomezz <jgomezz@gmail.com>
2024-09-22 11:05:02 -0500

    update

--- src/Ejercicio01.java
+++ src/Ejercicio01.java
@@ -1,15 +1,30 @@
-import java.security.DomainCombiner;
 
+
+/**
+ *  Primer ejerccio de la sesion 1
+ *  
+ *  Creacion de variables
+ *  
+ */
 public class Ejercicio01 {
 	
+	/**
+	 *  Programa principal
+	 * @param args
+	 * 
+	 */
 	public static void main(String[] args) {
 		
-		 String nombre = "Pedro";
-		 int numeroFavorito = 7;
-		 boolean esDomingo = true;
-		 int anhoActual = 2024;
+		// Definicion de nombre
+		String nombre = "Pedro";
+		
+		
+		int numeroFavorito = 7;  // mi numero favorito
+		boolean esDomingo = true;
+		int anhoActual = 2024;
 		 
-		 System.out.println("Mi nombre es " + nombre);
+		
+		System.out.println("Mi nombre es " + nombre);
 	}
 
 }

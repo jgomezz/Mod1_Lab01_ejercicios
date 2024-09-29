716228d5a42b39b2448672c1333ea44ad9155181
jgomezz <jgomezz@gmail.com>
2024-09-29 10:55:06 -0500

    update

---
+++ src/Ejercicio14_Array.java
@@ -0,0 +1,17 @@
+
+public class Ejercicio14_Array {
+
+	public static void main(String[] args) {
+
+//		int numeros[] = new int[10];
+//	
+//		numeros[0] = 12;
+		
+		int numeros[] = {12,0,0,0,0,0,0,0,0,0};
+		
+		System.out.println(numeros);
+		
+		System.out.println(numeros[0]);
+		
+	}
+}
